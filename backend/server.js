e73755a2df141113660318589dc4642cf9ace010
Luis-FP <luis_parparcen@yahoo.com>
2021-08-20 13:22:50 -0500

    subi memoria del body parsser tratando de solventar la falla de exceso de capacidad en S3

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,20 @@ if (pathDominio === "build") {
 }
 
 
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+
+// app.use(bodyParser.json({ limit: "3mb" }));
 app.use(bodyParser.urlencoded({
+  limit: '50mb',
   extended: true
 }));
 
-app.use(bodyParser.json({ limit: "3mb" }));
+app.use(bodyParser.json({
+  limit: '50mb', 
+  extended: true
+}));
 // cronograma de envio de emails de eficiencia
 
 app.use("/api/uploads", uploadRoute);
